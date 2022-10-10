a24b85c36e775aaabd2f1cf8c5825e75482d21c4
michael <michael@seooneclick.com>
2022-10-10 13:43:12 +0200

    Adds Popup ACF fields

--- template-parts/sections/general/modal-contact-form.php
+++ template-parts/sections/general/modal-contact-form.php
@@ -4,26 +4,39 @@
  *
  */
 
+$sectionData = get_field( 'contact_form_popup' );
 $allowedHtml = array(
   'br' => array(),
 );
 ?>
 <section class="section-contact-us-modal container">
-  <p class="fw-bold mb-6 fs-5 text-end"><a href="javascript:void(0)" type="button" class="btn-contact-us-modal text-uppercase" data-bs-toggle="modal" data-bs-target="#contact-us-modal">Contact us for the best possible price</a></p>
+  <?php
+  if ( ! empty( $sectionData['link_text'] ) ) :
+    ?>
+    <p class="fw-bold mb-6 fs-5 text-center text-md-end"><a href="javascript:void(0)" type="button" class="btn-contact-us-modal text-uppercase" data-bs-toggle="modal" data-bs-target="#contact-us-modal"><?php esc_html_e( $sectionData['link_text'] ); ?></a></p>
+    <?php
+  endif;
+  ?>
   <div class="modal fade" id="contact-us-modal" tabindex="-1" aria-labelledby="contact-us-modal-label" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg">
       <div class="modal-content">
         <div class="modal-header p-lg-6">
-          <h3 class="modal-title" id="contact-us-modal-label">Contact us for the best possible price</h3>
+          <?php
+          if ( ! empty( $sectionData['popup_title'] ) ) :
+            ?>
+            <h3 class="modal-title" id="contact-us-modal-label"><?php esc_html_e( $sectionData['popup_title'] ); ?></h3>
+            <?php
+          endif;
+          ?>
           <a type="button" class="btn-close text-danger" data-bs-dismiss="modal" aria-label="Close" href="javascript:void(0)"><i class="fas fa-times"></i></a>
         </div>
         <?php
-        if ( get_field( 'contact_form_shortcode' ) ) :
+        if ( ! empty( $sectionData['contact_form_shortcode'] ) ) :
           ?>
           <div class="modal-body p-lg-12">
             <div class="row justify-content-center">
               <div class="col-lg-10">
-                <?php echo do_shortcode( get_field( 'contact_form_shortcode' ) ); ?>
+                <?php echo do_shortcode( $sectionData['contact_form_shortcode'] ); ?>
               </div>
             </div>
           </div>
